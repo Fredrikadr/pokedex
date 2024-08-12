24b0f9acca1cc46437a7f3f73ae59237d2c553bb
Fredrik <fredrikadr@gmail.com>
2024-08-12 15:06:57 +0200

    add state management for Pokémon data; add error handling and loading indicators

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
 import { useState, useEffect } from 'react'
-import { getAllPokemonDetails } from './services/apiService';
+import { getAllPokemonDetails, getPokemonList } from './services/apiService';
 import './App.css'
+import { PokemonList, Pokemon } from './models/Pokemon';
 
 function App() {
 
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      
+      // Fetch list of pokemon and set pokemon list in state
+      try {
+        const list = await getPokemonList();
+        setPokemonList(list.results)
+      } catch (error) {
+        setError("Error fetching Pokemon data");
+        console.error("Error fetching Pokemon data:", error)
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+
     getAllPokemonDetails();
+
+
   }, []);
+
+  useEffect(() => {
+    console.log(pokemonList)
+  }, [pokemonList]);
 
   return (
     <>
+    {isLoading && <p>Loading...</p>}
+    {error && <p>{error}</p>}
       <p>Hello world</p>
     </>
   )
