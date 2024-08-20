e7b443f75674bcd3dc52ae7f7008fc484f4fccda
Fredrik <fredrikadr@gmail.com>
2024-08-20 10:16:41 +0200

    add handler for getting pokemon details

--- src/__tests__/mocks/handlers.ts
+++ src/__tests__/mocks/handlers.ts
@@ -1,32 +1,50 @@
 import { http } from 'msw';
-import { PokemonList } from '../../models/Pokemon';
+import { API_URL } from '../../constants';
+import { mockPokemonList, mockPokemonDetails } from './mockData';
 
-const mockPokemonList: PokemonList = {
-    count: 2,
-    next: null,
-    previous: null,
-    results: [
-        { name: 'Pikachu', url: 'https://pokeapi.co/api/v2/pokemon/25/' },
-        { name: 'Bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' }
-    ]
-}
+
+
+
 
 // Handler for fetching list of pokemon
 export const handlers = [
-    http.get('https://pokeapi.co/api/v2/pokemon', ({ request }) => {
-        console.log('Captured a request')
+    http.get(API_URL, ({ request }) => {
+        console.log('Captured a request: Pokemon list')
 
         const url = new URL(request.url);
         const limitParam = url.searchParams.get('limit');
         const limit = limitParam ? parseInt(limitParam) : 10; // Default to 10 if no limit
-        console.log(limitParam)
 
         const limitedResults = mockPokemonList.results.slice(0, limit);
+        if (limitedResults) {
+            return new Response(JSON.stringify({
+                ...mockPokemonList,
+                results: limitedResults
+            }))
+        }
+        else {
+            return new Response(JSON.stringify({ error: 'Pokemon list not found.' }), { status: 404 })
+        }
 
-        return new Response(JSON.stringify({
-            ...mockPokemonList,
-            results: limitedResults
-        }))
 
 
-    })]
+    }),
+
+    http.get(`${API_URL}/:id`, ({ params }) => {
+        console.log('Captured a request: pokemon id')
+        const { id } = params;
+        const pokemonId = parseInt(Array.isArray(id) ? id[0] : id); // Check if id is an array or not
+        const pokemon = mockPokemonDetails.find(p => p.id === pokemonId);
+
+        if (pokemon) {
+            return new Response(JSON.stringify({
+                pokemon
+
+            }
+            ))
+        }
+        else {
+            return new Response(JSON.stringify({ error: 'Pokemon not found.' }), { status: 404 })
+        }
+    })
+]
