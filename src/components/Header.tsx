fcca947c2aab0ac49e06c02f4d6d40169140316c
Fredrik <fredrikadr@gmail.com>
2024-10-15 17:02:19 +0200

    create header,footer and search bar components

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="px-5 drop-shadow-2xl bg-gradient-to-t from-light-red to-dark-red text-white h-32 flex items-center justify-center">
+            <h1 className="text-7xl">Pokédex</h1>
+        </header>
+    )
+}
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,6 @@
+export default function SearchBar() {
+    <div className="flex items-center justify-center sticky top-0">
+        <input className="p-2" placeholder="Search for pokemon"></input>
+        <div className="bg-dark-blue text-white">Search</div>
+    </div>
+}
