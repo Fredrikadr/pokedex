08027107d02a32b6d1bc52e8fe8b4005c7a2d7a2
Fredrik <fredrikadr@gmail.com>
2024-10-15 22:01:19 +0200

    add navigation to pokemon cards

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import { PokemonDetails } from "../models/Pokemon";
 
 type PokemonCardProps = {
   pokemon: PokemonDetails;
-  selectPokemon: (name: string) => void
+  /* selectPokemon: (name: string) => void */
 };
 
-export default function PokemonCard({ pokemon, selectPokemon }: PokemonCardProps) {
+
+
+export default function PokemonCard({ pokemon }: PokemonCardProps) {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/pokemon/${pokemon.name}`)
+  }
+
+
   return (
-    <div className=" bg-white  rounded-lg flex flex-col shadow-md group"
-      onClick={() => selectPokemon(pokemon.name)}>
+    <div className=" bg-white rounded-lg flex flex-col shadow-md group"
+      onClick={handleClick}>
       <div className="shadow-sm border rounded-b-lg w-8/12 text-center mx-auto p-2 mt-0">
         <h3 className="">{pokemon.name}</h3>
       </div>
