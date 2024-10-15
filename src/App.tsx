2d4a8e276a532bd68212ea9d65d00c20b8097535
Fredrik <fredrikadr@gmail.com>
2024-10-15 21:30:22 +0200

    add routes and refactored findAndAddDetails

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { PokemonList, Pokemon, PokemonDetails } from './models/Pokemon';
 import PokemonCard from './components/PokemonCard';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DetailsPage from './pages/DetailsPage';
+import HomePage from './pages/HomePage';
 
 
 function App() {
@@ -12,7 +15,7 @@ function App() {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   const [detailsList, setDetailsList] = useState<PokemonDetails[]>([]);
   const [searchList, setSearchList] = useState<Pokemon[]>([])
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [selectedPokemon, setSelectedPokemon] = useState<PokemonDetails | null>(null);
@@ -25,13 +28,13 @@ function App() {
 
       // Fetch list of pokemon and set pokemon list in state
       try {
-        const data = await getPokemonList(10);
+        const data = await getPokemonList(1025);
         setPokemonList(data.results);
+        console.log(pokemonList)
 
-        const speciesList = await getPokemonSpeciesList();
-        setSearchList(speciesList);
+        const slicedList = data.results.slice(0, 10)
 
-        const detailsData = await getAllPokemonDetails(data.results);
+        const detailsData = await getAllPokemonDetails(slicedList);
         setDetailsList(detailsData);
 
       } catch (error) {
@@ -51,46 +54,63 @@ function App() {
 
   }, [detailsList]);
 
-  const searchAndAddDetails = async (name: string) => {
-    const existingPokemon = detailsList.find(pokemon => pokemon.name === name);
+  const findAndAddDetails = async (name: string | undefined): Promise<PokemonDetails | null> => {
+    const existingPokemon = detailsList.find(pokemon => pokemon.name == name);
+    if(!name) {
+      return null;
+    }
 
-    if (!existingPokemon) {
-      const pokemonIndex = searchList.findIndex(pokemon => pokemon.name == name).toString()
-      const newDetailsData = await getPokemonDetails(pokemonIndex)
+    if (existingPokemon) {
+      return existingPokemon;
+    }
 
-      setDetailsList(prevDetailsList => [...prevDetailsList, newDetailsData])
-
-      return newDetailsData;
-    } else return existingPokemon;
+    const pokemon = pokemonList.find(pokemon => pokemon.name == name);
+    if (!pokemon) {
+      throw new Error("Couldn't find the selected pokemon")
+    }
+    const newDetailsData = await getPokemonDetails(pokemon.url)
+    setDetailsList(prevDetailsList => [...prevDetailsList, newDetailsData])
+    return newDetailsData;
 
   }
 
   const handleSelectPokemon = async (name: string) => {
-    setSelectedPokemon(await searchAndAddDetails(name))
+    setSelectedPokemon(await findAndAddDetails(name))
   }
 
   return (
     <>
       <Header />
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-
-      {selectedPokemon &&
-
-          <p>Selected pokemon: {selectedPokemon.name}</p>
-        }
-
-      {!selectedPokemon &&
-        
-          <main className="p-10" >
-            <div className="container mx-auto flex grid sm:grid-cols-1 md:grid-cols-4 gap-12">
-              {detailsList.map((pokemon, index) => (
-                <PokemonCard key={index} pokemon={pokemon} selectPokemon={handleSelectPokemon} />
-              ))}
-            </div>
-          </main>
-      }
-    <Footer />
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/pokemon/:name' element={<DetailsPage findAndAddDetails={findAndAddDetails} isLoading={isLoading} />} />
+        </Routes>
+      </Router>
     </>
-)}
-      export default App
+
+    /*  <>
+       <Header />
+       {isLoading && <p>Loading...</p>}
+       {error && <p>{error}</p>}
+ 
+       {selectedPokemon &&
+ 
+           <p>Selected pokemon: {selectedPokemon.name}</p>
+         }
+ 
+       {!selectedPokemon &&
+         
+           <main className="p-10" >
+             <div className="container mx-auto flex grid sm:grid-cols-1 md:grid-cols-4 gap-12">
+               {detailsList.map((pokemon, index) => (
+                 <PokemonCard key={index} pokemon={pokemon} selectPokemon={handleSelectPokemon} />
+               ))}
+             </div>
+           </main>
+       }
+     <Footer />
+     </> */
+  )
+}
+export default App
