b7108fd4579aead61d5014919d2930eac44e1b4c
Fredrik <fredrikadr@gmail.com>
2024-10-15 13:54:32 +0200

    add getPokemonSpecies function

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,7 @@
 import { API_URL } from "../constants";
 import { Pokemon, PokemonList } from "../models/Pokemon";
 
-// Fetch list of Pokemon
+// Fetch list of Pokemon with name and url with basic details
 export const getPokemonList = async (limit: number): Promise<PokemonList> => {
     try {
       const response = await fetch(`${API_URL}/?limit=${limit}`);
@@ -15,6 +15,21 @@ export const getPokemonList = async (limit: number): Promise<PokemonList> => {
       throw error;
     }
   };
+
+  // Fetch full list of pokemon names and url with more details - used for search
+export const getPokemonSpeciesList = async () => {
+  try {
+    const response = await fetch(`${API_URL}-species/?offset=0&limit=1025`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    console.error(`Error fetching pokemon list`, error)
+    throw error;
+  }
+}
   
   // Fetch details for 1 Pokemon
   export const getPokemonDetails = async (url: string) => {
