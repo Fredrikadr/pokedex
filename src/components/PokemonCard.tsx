6af5534a98d3cb1684254bd806a641a168d89ef5
Fredrik <fredrikadr@gmail.com>
2024-10-15 13:55:04 +0200

    fix key issue in PokemonCard and add hover animation

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,16 @@
 import { PokemonDetails } from "../models/Pokemon";
 
 type PokemonCardProps = {
-    pokemon: PokemonDetails;
-    index: number;
-  };
+  pokemon: PokemonDetails;
+};
 
-export default function PokemonCard({pokemon, index}: PokemonCardProps) {
-    return (
-        <div className=" bg-white  rounded-lg flex flex-col shadow-md">
-            <div className="shadow-sm border rounded-b-lg w-8/12 text-center mx-auto p-2 mt-0">
-            <h3 className="" key={index}>{pokemon.name}</h3>
-            </div>
-            <img className="w-56 m-auto" src={pokemon.sprites.front_default}></img>
-          </div>
-    )
+export default function PokemonCard({ pokemon }: PokemonCardProps) {
+  return (
+    <div className=" bg-white  rounded-lg flex flex-col shadow-md group">
+      <div className="shadow-sm border rounded-b-lg w-8/12 text-center mx-auto p-2 mt-0">
+        <h3 className="">{pokemon.name}</h3>
+      </div>
+      <img className=" w-56 m-auto transition-transform duration-300 ease-in-out transform group-hover:scale-110" src={pokemon.sprites.front_default}></img>
+    </div>
+  )
 }
