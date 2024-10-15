4db6be0972122564e5409fdaba92e2d3ee283fe7
Fredrik <fredrikadr@gmail.com>
2024-10-15 13:53:57 +0200

    add header + footer, add searchList and searchQuery to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { getAllPokemonDetails, getPokemonList } from './services/apiService';
+import { getAllPokemonDetails, getPokemonList, getPokemonSpeciesList } from './services/apiService';
 import './App.css'
 import { PokemonList, Pokemon, PokemonDetails } from './models/Pokemon';
 import PokemonCard from './components/PokemonCard';
@@ -8,9 +8,12 @@ function App() {
 
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   const [detailsList, setDetailsList] = useState<PokemonDetails[]>([]);
+  const [searchList, setSearchList] = useState<PokemonList[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
+  // Set initial list of pokemon to populate the page and get list of all pokemon for search
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -21,8 +24,10 @@ function App() {
         const data = await getPokemonList(10);
         setPokemonList(data.results);
 
-        const detailsData = await getAllPokemonDetails(data.results);
+        const speciesList = await getPokemonSpeciesList();
+        setSearchList(speciesList);
 
+        const detailsData = await getAllPokemonDetails(data.results);
         setDetailsList(detailsData);
 
       } catch (error) {
@@ -43,16 +48,28 @@ function App() {
   }, [detailsList]);
 
   return (
-    <main>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      <div className="container mx-auto flex grid sm:grid-cols-1 md:grid-cols-4 gap-10">
-                {detailsList.map((pokemon, index) => (
-          <PokemonCard index={index} pokemon={pokemon}/>
-        ))}
-      </div>
+    <>
+      <header className="px-5 drop-shadow-2xl bg-gradient-to-t from-light-red to-dark-red text-white h-32 flex items-center justify-center">
+        <h1 className="text-7xl">Pokédex</h1>
+      </header>
 
+      <div className="flex items-center justify-center sticky top-0">
+      <input className="p-2" placeholder="Search for pokemon"></input>
+      <div className="bg-dark-blue text-white">Search</div>
+      </div>
+      
+      <main className="p-10" >
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        <div className="container mx-auto flex grid sm:grid-cols-1 md:grid-cols-4 gap-12">
+          {detailsList.map((pokemon, index) => (
+            <PokemonCard key={index} pokemon={pokemon}/>
+          ))}
+        </div>
       </main>
+      <footer className="h-20 bg-dark-red"></footer>
+
+    </>
 
   )
 };
