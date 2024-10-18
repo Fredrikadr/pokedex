1901bdfe949838ba14efc76415dee25e4608dc2f
Fredrik <fredrikadr@gmail.com>
2024-10-18 21:18:30 +0200

    cleanup in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { useState, useEffect } from 'react'
-import { getAllPokemonDetails, getPokemonDetails, getPokemonList, getPokemonSpeciesList } from './services/apiService';
+import { getAllPokemonDetails, getPokemonDetails, getPokemonList } from './services/apiService';
 import './App.css'
-import { PokemonList, Pokemon, PokemonDetails } from './models/Pokemon';
-import PokemonCard from './components/PokemonCard';
+import { Pokemon, PokemonDetails } from './models/Pokemon';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DetailsPage from './pages/DetailsPage';
 import HomePage from './pages/HomePage';
+import SearchBar from './components/SearchBar';
+import PageNotFound from './pages/PageNotFound';
 
 
 function App() {
 
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   const [detailsList, setDetailsList] = useState<PokemonDetails[]>([]);
-  const [searchList, setSearchList] = useState<Pokemon[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [selectedPokemon, setSelectedPokemon] = useState<PokemonDetails | null>(null);
+ 
 
   // Set initial list of pokemon to populate the page and get list of all pokemon for search
   useEffect(() => {
@@ -56,7 +56,7 @@ function App() {
 
   const findAndAddDetails = async (name: string | undefined): Promise<PokemonDetails | null> => {
     const existingPokemon = detailsList.find(pokemon => pokemon.name == name);
-    if(!name) {
+    if (!name) {
       return null;
     }
 
@@ -74,43 +74,22 @@ function App() {
 
   }
 
-  const handleSelectPokemon = async (name: string) => {
-    setSelectedPokemon(await findAndAddDetails(name))
-  }
 
   return (
     <>
-      <Header />
-      <Router>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/pokemon/:name' element={<DetailsPage findAndAddDetails={findAndAddDetails} isLoading={isLoading} />} />
-        </Routes>
-      </Router>
+      <div className="flex flex-col min-h-screen">
+        <Router>
+        <Header />
+        <SearchBar pokemonList={pokemonList}/>
+          <Routes>
+            <Route path='/' element={<HomePage detailsList={detailsList} />} />
+            <Route path='/pokemon/:name' element={<DetailsPage findAndAddDetails={findAndAddDetails} isLoading={isLoading} />} />
+            <Route path='*' element={<PageNotFound/>} />
+          </Routes>
+        <Footer />
+        </Router>
+      </div>
     </>
-
-    /*  <>
-       <Header />
-       {isLoading && <p>Loading...</p>}
-       {error && <p>{error}</p>}
- 
-       {selectedPokemon &&
- 
-           <p>Selected pokemon: {selectedPokemon.name}</p>
-         }
- 
-       {!selectedPokemon &&
-         
-           <main className="p-10" >
-             <div className="container mx-auto flex grid sm:grid-cols-1 md:grid-cols-4 gap-12">
-               {detailsList.map((pokemon, index) => (
-                 <PokemonCard key={index} pokemon={pokemon} selectPokemon={handleSelectPokemon} />
-               ))}
-             </div>
-           </main>
-       }
-     <Footer />
-     </> */
   )
 }
 export default App
